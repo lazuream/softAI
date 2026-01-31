7b97b4ba0a205feab4f871ac5df20cc37b78e974
LazureaM <lazuream2004@gmail.com>
2026-02-01 01:37:23 +0800

    first commit

---
+++ src/main/java/com/lazuream/Main.java
@@ -0,0 +1,12 @@
+package com.lazuream;
+
+/**
+ * @author lazuream
+ * @Description ${DESCRIPTION}
+ * @create 2026/2/1
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
