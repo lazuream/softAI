1181adedfddee255a0039737d1bed4c911890a1e
LazureaM <lazuream2004@gmail.com>
2026-02-01 01:59:48 +0800

    test1

--- src/main/java/com/lazuream/Main.java
+++ src/main/java/com/lazuream/Main.java
@@ -7,6 +7,6 @@ package com.lazuream;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1!");
     }
 }
