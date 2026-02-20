d2a597647d3aadafa2ebf4c077e9dcaed6b791a3
LazureaM <lazuream2004@gmail.com>
2026-02-20 19:17:10 +0800

    Add README.md

---
+++ src/main/java/com/lazuream/ai/SoftaiApplication.java
@@ -0,0 +1,13 @@
+package com.lazuream.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftaiApplication.class, args);
+	}
+
+}
